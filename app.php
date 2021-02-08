783bed2d4fa4aa5f5da4d372b3bbbdba8a53dd23
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-08 22:44:26 +0300

    Update app.php

--- app.php
+++ app.php
@@ -2,6 +2,26 @@
 
 $age = 25;
 
+
+/**
+ * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
+ * yaş grubu tespit edilip ekrana bu yaş grubunun ismi yazılmalıdır. 
+ * 
+ * 0-14 = Çocuk
+ * 15-24 = Genç
+ * 25-64 = Yetişkin
+ * 65+ = Yaşlı
+ * 
+ * Ekran çıktısı için `echo` yada `print` kullanabilirsiniz.
+ * 
+ * > **Not**: Çıktıda sadece `Çocuk`, `Genç`, `Yetişkin` veya `Yaşlı`
+ * yer almalıdır.
+ * 
+ * İyi çalışmalar.
+ */
+
+
+
 if ($age<=14):
 	print_r("Çocuk");
 elseif ($age<=24):
@@ -10,10 +30,7 @@ elseif ($age<=64):
 	print_r("Yetişkin");
 elseif ($age>=65):
 	print_r("Yaşlı");
-else:
-	print_r("İhtiyar");
 endif;
 
 ?>
-
 
